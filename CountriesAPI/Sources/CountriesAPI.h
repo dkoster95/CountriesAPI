4eb6737617e8d8380bf48638bdf470dbfd8c31ae
Daniel Koster <danielkoster95@gmail.com>
2021-05-11 20:08:40 -0400

    Framework extracted to this Git

---
+++ CountriesAPI/Sources/CountriesAPI.h
@@ -0,0 +1,18 @@
+//
+//  CountriesAPI.h
+//  CountriesAPI
+//
+//  Created by Daniel Koster on 5/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CountriesAPI.
+FOUNDATION_EXPORT double CountriesAPIVersionNumber;
+
+//! Project version string for CountriesAPI.
+FOUNDATION_EXPORT const unsigned char CountriesAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CountriesAPI/PublicHeader.h>
+
+
